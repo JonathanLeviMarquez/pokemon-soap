8f31c15870d7c3292ffd2b7f573b1859d9b0daec
JonathanLeviMarquez <jonathanlevimarquez@gmail.com>
2025-10-20 14:46:30 -0600

    Add files via upload

---
+++ src/main/java/com/bancaya/challenge/pokemon_soap/PokemonSoapApplication.java
@@ -0,0 +1,13 @@
+package com.bancaya.challenge.pokemon_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancaya/challenge/pokemon_soap/PokemonSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancaya.challenge.pokemon_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
